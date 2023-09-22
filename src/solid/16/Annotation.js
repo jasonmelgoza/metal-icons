378c1ed182ebcda68ae5aa26ddf3b8659cd53f99
Jason Melgoza <jasonmelgoza@gmail.com>
2023-09-22 16:47:53 -0700

    Added annotation icon

---
+++ src/solid/16/Annotation.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+const SvgAnnotation = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={16}
+    fill="none"
+    viewBox="0 0 16 16"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M12.75 1A2.25 2.25 0 0 1 15 3.25v6.5A2.25 2.25 0 0 1 12.75 12H10.5l-1.887 2.68a.75.75 0 0 1-1.226 0L5.5 12H3.25A2.25 2.25 0 0 1 1 9.75v-6.5A2.25 2.25 0 0 1 3.25 1h9.5Z"
+    />
+  </svg>
+)
+export default SvgAnnotation
---
+++ src/solid/24/Annotation.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+const SvgAnnotation = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M20.25 3A2.75 2.75 0 0 1 23 5.75v9.5A2.75 2.75 0 0 1 20.25 18h-4.5l-2.83 3.519a1.125 1.125 0 0 1-1.84 0L8.25 18H4.375A3.375 3.375 0 0 1 1 14.625V5.75A2.75 2.75 0 0 1 3.75 3h16.5Z"
+    />
+  </svg>
+)
+export default SvgAnnotation
