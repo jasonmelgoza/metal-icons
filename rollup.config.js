e0fc285d42720151d638c1c6b478373ebc38e0bd
Jason Melgoza <jasonmelgoza@gmail.com>
2023-10-28 22:15:49 -0700

    Changed input names

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,13 @@ import { babel } from '@rollup/plugin-babel'
 
 const config = {
   input: {
-    '16/outline/index': 'src/16/outline/index.js',
-    '16/solid/index': 'src/16/solid/index.js',
-    '24/outline/index': 'src/24/outline/index.js',
-    '24/solid/index': 'src/24/solid/index.js',
+    '16/outline/index.esm': 'src/16/outline/index.js',
+    '16/solid/index.esm': 'src/16/solid/index.js',
+    '24/outline/index.esm': 'src/24/outline/index.js',
+    '24/solid/index.esm': 'src/24/solid/index.js',
   },
   output: {
-    dir: 'dist/index.esm.js',
+    dir: 'dist',
     entryFileNames: '[name].js',
     format: 'esm',
   },
