9d000e0f5433f144dda5c83a81d706beb1618416
Jason Melgoza <jasonmelgoza@gmail.com>
2023-10-28 21:12:42 -0700

    Added all input folders to rollup

---
+++ dist/index.esm.js/24/solid/index.js
@@ -0,0 +1,104 @@
+import _extends from '@babel/runtime/helpers/extends';
+import * as React from 'react';
+
+const SvgActivity = props => /*#__PURE__*/React.createElement("svg", _extends({
+  xmlns: "http://www.w3.org/2000/svg",
+  width: 24,
+  height: 24,
+  fill: "none",
+  viewBox: "0 0 24 24"
+}, props), /*#__PURE__*/React.createElement("path", {
+  fill: "currentColor",
+  fillRule: "evenodd",
+  d: "M9.003 2a.75.75 0 0 1 .71.519l5.278 16.27 2.294-7.265A.75.75 0 0 1 18 11h4a.75.75 0 1 1 0 1.5h-3.45l-2.835 8.976a.75.75 0 0 1-1.428.005L8.99 5.151l-2.278 6.836A.75.75 0 0 1 6 12.5H2A.75.75 0 1 1 2 11h3.46l2.828-8.487A.75.75 0 0 1 9.003 2Z"
+}));
+
+const SvgAlert = props => /*#__PURE__*/React.createElement("svg", _extends({
+  xmlns: "http://www.w3.org/2000/svg",
+  width: 16,
+  height: 16,
+  fill: "currentColor",
+  viewBox: "0 0 16 16"
+}, props), /*#__PURE__*/React.createElement("path", {
+  fillRule: "evenodd",
+  d: "M8.541 1.07a2.143 2.143 0 0 0-1.588.203l.001.001c-.32.179-.589.437-.78.75l-.002.004-5.878 9.764-.006.01a2.125 2.125 0 0 0 .768 2.904c.321.189.686.29 1.059.294h11.77a2.148 2.148 0 0 0 1.06-.293 2.126 2.126 0 0 0 .768-2.905L9.829 2.028l-.002-.004a2.143 2.143 0 0 0-1.286-.954Zm-1.248 9.223A1 1 0 0 0 8 12h.007a1 1 0 0 0 0-2H8a1 1 0 0 0-.707.293ZM8.53 5.22c.141.14.22.331.22.53v2.5a.75.75 0 0 1-1.5 0v-2.5a.75.75 0 0 1 1.28-.53Z"
+}));
+
+const SvgAlignCenter = props => /*#__PURE__*/React.createElement("svg", _extends({
+  xmlns: "http://www.w3.org/2000/svg",
+  width: 24,
+  height: 24,
+  fill: "currentColor",
+  viewBox: "0 0 24 24"
+}, props), /*#__PURE__*/React.createElement("path", {
+  d: "M2.75 5a.75.75 0 0 0 0 1.5h18.5a.75.75 0 1 0 0-1.5H2.75Zm3 4a.75.75 0 0 0 0 1.5h12.5a.75.75 0 1 0 0-1.5H5.75ZM2 13.75a.75.75 0 0 1 .75-.75h18.5a.75.75 0 1 1 0 1.5H2.75a.75.75 0 0 1-.75-.75ZM5.75 17a.75.75 0 1 0 0 1.5h12.5a.75.75 0 1 0 0-1.5H5.75Z"
+}));
+
+const SvgAlignJustify = props => /*#__PURE__*/React.createElement("svg", _extends({
+  xmlns: "http://www.w3.org/2000/svg",
+  width: 24,
+  height: 24,
+  fill: "currentColor",
+  viewBox: "0 0 24 24"
+}, props), /*#__PURE__*/React.createElement("path", {
+  d: "M2.75 5a.75.75 0 0 0 0 1.5h18.5a.75.75 0 1 0 0-1.5H2.75Zm0 4a.75.75 0 0 0 0 1.5h18.5a.75.75 0 1 0 0-1.5H2.75ZM2 13.75a.75.75 0 0 1 .75-.75h18.5a.75.75 0 1 1 0 1.5H2.75a.75.75 0 0 1-.75-.75ZM2.75 17a.75.75 0 1 0 0 1.5h18.5a.75.75 0 1 0 0-1.5H2.75Z"
+}));
+
+const SvgAlignLeft = props => /*#__PURE__*/React.createElement("svg", _extends({
+  xmlns: "http://www.w3.org/2000/svg",
+  width: 24,
+  height: 24,
+  fill: "currentColor",
+  viewBox: "0 0 24 24"
+}, props), /*#__PURE__*/React.createElement("path", {
+  d: "M2.75 5a.75.75 0 0 0 0 1.5h18.5a.75.75 0 1 0 0-1.5H2.75Zm0 4a.75.75 0 0 0 0 1.5h14.5a.75.75 0 1 0 0-1.5H2.75ZM2 13.75a.75.75 0 0 1 .75-.75h18.5a.75.75 0 1 1 0 1.5H2.75a.75.75 0 0 1-.75-.75ZM2.75 17a.75.75 0 1 0 0 1.5h14.5a.75.75 0 1 0 0-1.5H2.75Z"
+}));
+
+const SvgAlignRight = props => /*#__PURE__*/React.createElement("svg", _extends({
+  xmlns: "http://www.w3.org/2000/svg",
+  width: 24,
+  height: 24,
+  fill: "currentColor",
+  viewBox: "0 0 24 24"
+}, props), /*#__PURE__*/React.createElement("path", {
+  d: "M2.75 5a.75.75 0 0 0 0 1.5h18.5a.75.75 0 1 0 0-1.5H2.75Zm4 4a.75.75 0 0 0 0 1.5h14.5a.75.75 0 1 0 0-1.5H6.75ZM2 13.75a.75.75 0 0 1 .75-.75h18.5a.75.75 0 1 1 0 1.5H2.75a.75.75 0 0 1-.75-.75ZM6.75 17a.75.75 0 1 0 0 1.5h14.5a.75.75 0 1 0 0-1.5H6.75Z"
+}));
+
+const SvgAnnotation = props => /*#__PURE__*/React.createElement("svg", _extends({
+  xmlns: "http://www.w3.org/2000/svg",
+  width: 24,
+  height: 24,
+  fill: "none",
+  viewBox: "0 0 24 24"
+}, props), /*#__PURE__*/React.createElement("path", {
+  fill: "currentColor",
+  d: "M20.25 3A2.75 2.75 0 0 1 23 5.75v9.5A2.75 2.75 0 0 1 20.25 18h-4.5l-2.83 3.519a1.125 1.125 0 0 1-1.84 0L8.25 18H4.375A3.375 3.375 0 0 1 1 14.625V5.75A2.75 2.75 0 0 1 3.75 3h16.5Z"
+}));
+
+const SvgArchive = props => /*#__PURE__*/React.createElement("svg", _extends({
+  xmlns: "http://www.w3.org/2000/svg",
+  width: 24,
+  height: 24,
+  fill: "none",
+  viewBox: "0 0 24 24"
+}, props), /*#__PURE__*/React.createElement("path", {
+  fill: "currentColor",
+  d: "M1 4.25A2.25 2.25 0 0 1 3.25 2h17.5A2.25 2.25 0 0 1 23 4.25v1.5c0 .78-.396 1.466-.999 1.87H1.998A2.248 2.248 0 0 1 1 5.75v-1.5Z"
+}), /*#__PURE__*/React.createElement("path", {
+  fill: "currentColor",
+  fillRule: "evenodd",
+  d: "M2 9h20v10.25A2.75 2.75 0 0 1 19.25 22H4.75A2.75 2.75 0 0 1 2 19.25V9Zm7.22 3.22a.75.75 0 0 1 .53-.22h4.5a.75.75 0 1 1 0 1.5h-4.5a.75.75 0 0 1-.53-1.28Z"
+}));
+
+const SvgFolder = props => /*#__PURE__*/React.createElement("svg", _extends({
+  xmlns: "http://www.w3.org/2000/svg",
+  width: 24,
+  height: 24,
+  fill: "none",
+  viewBox: "0 0 24 24"
+}, props), /*#__PURE__*/React.createElement("path", {
+  fill: "currentColor",
+  d: "M1.805 2.805A2.75 2.75 0 0 1 3.75 2h5.672c.73 0 1.428.29 1.944.805l.829.829c.234.234.552.366.883.366h7.172A2.75 2.75 0 0 1 23 6.75V7H1V4.75c0-.73.29-1.429.805-1.945ZM1 8.5v10.75A2.75 2.75 0 0 0 3.75 22h16.5A2.75 2.75 0 0 0 23 19.25V8.5H1Z"
+}));
+
+export { SvgActivity as Activity, SvgAlert as Alert, SvgAlignCenter as AlignCenter, SvgAlignJustify as AlignJustify, SvgAlignLeft as AlignLeft, SvgAlignRight as AlignRight, SvgAnnotation as Annotation, SvgArchive as Archive, SvgFolder as Folder };
--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,15 @@
 import { babel } from '@rollup/plugin-babel'
 
 const config = {
-  input: 'src/index.js',
+  input: {
+    '16/outline/index': 'src/16/outline/index.js',
+    '16/solid/index': 'src/16/solid/index.js',
+    '24/outline/index': 'src/24/outline/index.js',
+    '24/solid/index': 'src/24/solid/index.js',
+  },
   output: {
     dir: 'dist/index.esm.js',
+    entryFileNames: '[name].js',
     format: 'esm',
   },
   external: [/@babel\/runtime/, 'react'],
