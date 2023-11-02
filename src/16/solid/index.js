55c880fcb55a65b2e4e4effecc737116c04db55c
Jason Melgoza <jasonmelgoza@gmail.com>
2023-11-02 07:04:34 -0700

    Added AlignBottom and AlignTop icons

--- src/16/solid/index.js
+++ src/16/solid/index.js
@@ -1,6 +1,8 @@
 export { default as Activity } from './Activity'
 export { default as Alarm } from './Alarm'
 export { default as Alert } from './Alert'
+export { default as AlignBottom } from './AlignBottom'
+export { default as AlignTop } from './AlignTop'
 export { default as Annotation } from './Annotation'
 export { default as Archive } from './Archive'
 export { default as Folder } from './Folder'
