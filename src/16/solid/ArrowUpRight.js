4dc1b667dfc751d25d5805658beeeb2a0c7ddfd3
Jason Melgoza <jasonmelgoza@gmail.com>
2023-11-06 22:37:21 -0800

    Added Arrow icon sets

---
+++ src/16/solid/ArrowUpRight.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+const SvgArrowUpRight = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={16}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M12.943 3.463A.748.748 0 0 0 12.25 3h-5.5a.75.75 0 0 0 0 1.5h3.69l-7.22 7.22a.75.75 0 1 0 1.06 1.06l7.22-7.22v3.69a.75.75 0 1 0 1.5 0v-5.5a.746.746 0 0 0-.057-.287Z"
+    />
+  </svg>
+)
+export default SvgArrowUpRight
