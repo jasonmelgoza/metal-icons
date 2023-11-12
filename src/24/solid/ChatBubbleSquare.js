72ffbf5152173c2fc94f8e4fb067473cb0908fda
Jason Melgoza <jasonmelgoza@gmail.com>
2023-11-12 10:33:26 -0800

    Added ChatBubbleSquare icons

---
+++ src/24/solid/ChatBubbleSquare.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+const SvgChatBubbleSquare = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="currentColor"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      d="M22 4.75A2.75 2.75 0 0 0 19.25 2H4.75A2.75 2.75 0 0 0 2 4.75v16.5a.75.75 0 0 0 1.231.575l4.228-3.534A1.25 1.25 0 0 1 8.26 18h10.99A2.75 2.75 0 0 0 22 15.25V4.75Z"
+    />
+  </svg>
+)
+export default SvgChatBubbleSquare
