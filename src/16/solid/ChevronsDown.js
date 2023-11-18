317e3d348b8aaeb93caeeb73370837774d2b5ac1
Jason Melgoza <jasonmelgoza@gmail.com>
2023-11-18 07:13:08 +0000

    Update 60 files

---
+++ src/16/solid/ChevronsDown.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+const SvgChevronsDown = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={16}
+    fill="currentColor"
+    {...props}
+  >
+    <path d="M5.3 3.24a.75.75 0 0 0-1.1 1.02l3.25 3.5a.75.75 0 0 0 1.1 0l3.25-3.5a.75.75 0 1 0-1.1-1.02L8 6.148 5.3 3.24Z" />
+    <path d="M5.3 8.24a.75.75 0 0 0-1.1 1.02l3.25 3.5a.752.752 0 0 0 1.1 0l3.25-3.5a.75.75 0 1 0-1.1-1.02L8 11.148 5.3 8.24Z" />
+  </svg>
+)
+export default SvgChevronsDown
---
+++ src/24/outline/ChevronUp.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+const SvgChevronUp = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="currentColor"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      d="M5.23 15.79a.75.75 0 0 0 1.06-.02L12 9.832l5.71 5.938a.75.75 0 1 0 1.08-1.04l-6.25-6.5a.749.749 0 0 0-1.08 0l-6.25 6.5a.75.75 0 0 0 .02 1.06Z"
+    />
+  </svg>
+)
+export default SvgChevronUp
