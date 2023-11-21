c9f976013f04e549f425338aa023aadd50757ff1
Jason Melgoza <jasonmelgoza@gmail.com>
2023-11-21 23:38:35 +0000

    Added CircleCaretUp icons

---
+++ packages/metal-icons/src/16/outline/CircleCaretUp.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+const SvgCircleCaretUp = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={16}
+    fill="currentColor"
+    {...props}
+  >
+    <path d="M5.092 9.61a.75.75 0 0 1 .027-.766l2.25-3.5a.75.75 0 0 1 1.262 0l2.25 3.5a.75.75 0 0 1-.63 1.156h-4.5a.75.75 0 0 1-.659-.39" />
+    <path
+      fillRule="evenodd"
+      d="M8 16A8 8 0 1 1 8 0a8 8 0 0 1 0 16m0-1.5a6.5 6.5 0 1 1 0-13 6.5 6.5 0 0 1 0 13"
+    />
+  </svg>
+)
+export default SvgCircleCaretUp
