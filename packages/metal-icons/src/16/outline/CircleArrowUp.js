477fce5cac051da199e3df04db58f23492fcafc1
Jason Melgoza <jasonmelgoza@gmail.com>
2023-11-20 21:26:31 -0800

    Added CircleArrowUp

---
+++ packages/metal-icons/src/16/outline/CircleArrowUp.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+const SvgCircleArrowUp = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={16}
+    fill="currentColor"
+    {...props}
+  >
+    <path d="M4.695 7.755a.75.75 0 0 0 1.06.05l1.495-1.36v4.805a.75.75 0 1 0 1.5 0V6.445l1.495 1.36a.75.75 0 0 0 1.01-1.11l-2.75-2.5a.75.75 0 0 0-1.01 0l-2.75 2.5a.75.75 0 0 0-.05 1.06Z" />
+    <path
+      fillRule="evenodd"
+      d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-6.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13"
+    />
+  </svg>
+)
+export default SvgCircleArrowUp
