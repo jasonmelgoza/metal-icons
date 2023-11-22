a1f061215c5c05b04acf9a624059509125045d6e
Jason Melgoza <jasonmelgoza@gmail.com>
2023-11-22 15:16:45 +0000

    Added CircleCheck icon

---
+++ packages/metal-icons/src/16/solid/CircleCheck.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+const SvgCircleCheck = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={16}
+    fill="currentColor"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m2.72 5.22a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-2-2a.75.75 0 0 1 1.06-1.06l1.47 1.47 3.97-3.97Z"
+    />
+  </svg>
+)
+export default SvgCircleCheck
