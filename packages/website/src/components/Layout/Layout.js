b0ced24518476e0b930fa0f21d8850759f90d5de
Jason Melgoza <jasonmelgoza@gmail.com>
2023-11-27 15:35:35 +0000

    feat: Change favicon
    Change favicon from favicon.ico to a custom emoji icon

--- packages/website/src/components/Layout/Layout.js
+++ packages/website/src/components/Layout/Layout.js
@@ -15,7 +15,10 @@ export default function Layout({ children }) {
           content="Introducing Metal Icons: a versatile icon library designed for your upcoming Figma or React projects."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤘</text></svg>"
+        />
       </Head>
       <div className={`${inter.className} ${Styles.Layout}`}>
         <header className={Styles.Header}>
