0c303b5477c2f202f6b1f66d13c8c0c4191e9235
Jason Melgoza <jasonmelgoza@gmail.com>
2023-11-28 05:16:20 +0000

    feat: Added export for CornerDownRight
    Added export for CornerDownRight component in multiple sizes and variants.

---
+++ packages/metal-icons/src/24/solid/CornerDownRight.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+const SvgCornerDownRight = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="currentColor"
+    {...props}
+  >
+    <path d="M2.5 3.75a.75.75 0 0 1 1.5 0v7c0 .847.357 1.669 1.009 2.282A3.64 3.64 0 0 0 7.5 14h10.878l-4.146-3.957a.75.75 0 1 1 1.036-1.086l5.498 5.25a.75.75 0 0 1 .002 1.085l-5.5 5.25a.75.75 0 1 1-1.036-1.085l4.146-3.957H7.5a5.139 5.139 0 0 1-3.52-1.375A4.634 4.634 0 0 1 2.5 10.75z" />
+  </svg>
+)
+export default SvgCornerDownRight
