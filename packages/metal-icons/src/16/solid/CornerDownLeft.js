da08cf1de8dc6b933a9b753ae999bb8a0cd0cbf5
Jason Melgoza <jasonmelgoza@gmail.com>
2023-11-28 04:30:22 +0000

    feat: Added CornerDownLeft icon in multiple sizes
    Added CornerDownLeft icon in 16px and 24px sizes for both outline and solid styles

---
+++ packages/metal-icons/src/16/solid/CornerDownLeft.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+const SvgCornerDownLeft = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={16}
+    fill="currentColor"
+    {...props}
+  >
+    <path d="M13.5 2.75a.75.75 0 1 0-1.5 0v4.5A1.75 1.75 0 0 1 10.25 9H4.56l2.22-2.22a.75.75 0 0 0-1.06-1.06l-3.5 3.5a.748.748 0 0 0 0 1.06l3.5 3.5a.75.75 0 0 0 1.06-1.06L4.56 10.5h5.69a3.25 3.25 0 0 0 3.25-3.25z" />
+  </svg>
+)
+export default SvgCornerDownLeft
