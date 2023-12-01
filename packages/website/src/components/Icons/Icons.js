41e46b3d692594ae37efe2ec471dc9ed78578406
Jason Melgoza <jasonmelgoza@gmail.com>
2023-12-01 18:10:21 +0000

    feat: Added large icons and styling
    Added large solid and outline icons with corresponding components and added styling for icon sections

--- packages/website/src/components/Icons/Icons.js
+++ packages/website/src/components/Icons/Icons.js
@@ -1,6 +1,8 @@
 import * as Tabs from '@radix-ui/react-tabs'
 import IconsSmallSolid from './IconsSmallSolid'
+import IconsLargeSolid from './IconsLargeSolid'
 import IconsSmallOutline from './IconsSmallOutline'
+import IconsLargeOutline from './IconsLargeOutline'
 import Styles from './Icons.module.css'
 
 export default function Icons({}) {
@@ -19,14 +21,40 @@ export default function Icons({}) {
           </Tabs.Trigger>
         </Tabs.List>
         <Tabs.Content className={Styles.TabsContent} value="tab1">
-          <div className={Styles.Grid}>
-            <IconsSmallSolid />
-          </div>
+          <section className={Styles.Section}>
+            <header className={Styles.Header}>
+              <h3>16px</h3>
+            </header>
+            <div className={Styles.Grid}>
+              <IconsSmallSolid />
+            </div>
+          </section>
+          <section className={Styles.Section}>
+            <header className={Styles.Header}>
+              <h3>24px</h3>
+            </header>
+            <div className={Styles.Grid}>
+              <IconsLargeSolid />
+            </div>
+          </section>
         </Tabs.Content>
         <Tabs.Content className={Styles.TabsContent} value="tab2">
-          <div className={Styles.Grid}>
-            <IconsSmallOutline />
-          </div>
+          <section className={Styles.Section}>
+            <header className={Styles.Header}>
+              <h3>16px</h3>
+            </header>
+            <div className={Styles.Grid}>
+              <IconsSmallOutline />
+            </div>
+          </section>
+          <section className={Styles.Section}>
+            <header className={Styles.Header}>
+              <h3>24px</h3>
+            </header>
+            <div className={Styles.Grid}>
+              <IconsLargeOutline />
+            </div>
+          </section>
         </Tabs.Content>
       </Tabs.Root>
     </>
