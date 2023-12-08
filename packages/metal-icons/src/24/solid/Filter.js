ff4878cd963b590c1c6430683b0dc57e112102e2
Jason Melgoza <jasonmelgoza@gmail.com>
2023-12-08 01:52:56 +0000

    feat: Added Filter icons
    Added Filter icons to metal-icons package in 16px and 24px sizes

---
+++ packages/metal-icons/src/24/solid/Filter.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+const SvgFilter = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="currentColor"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      d="M20.722 7.472c.258-.258.403-.607.403-.972V3.75A2.75 2.75 0 0 0 18.375 1H5.75A2.75 2.75 0 0 0 3 3.75V6.5c0 .365.145.714.403.972L9 14v8a1 1 0 0 0 1 1h1.5a1 1 0 0 0 .8-.4L15 19v-5z"
+    />
+  </svg>
+)
+export default SvgFilter
