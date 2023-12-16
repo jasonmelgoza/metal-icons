64fef6ac23bc074ac876d4fae27fdb4de38e0cf5
Jason Melgoza <jasonmelgoza@gmail.com>
2023-12-16 19:27:57 +0000

    feat: Added FolderMinus icon
    Added FolderMinus icon in multiple sizes and styles

---
+++ packages/metal-icons/src/16/solid/FolderMinus.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+const SvgFolderMinus = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={16}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M2.25 1A2.25 2.25 0 0 0 0 3.25v9.5A2.25 2.25 0 0 0 2.25 15h11.5A2.25 2.25 0 0 0 16 12.75v-7.5A2.25 2.25 0 0 0 13.75 3H9.871a.75.75 0 0 1-.53-.22L8.22 1.66A2.25 2.25 0 0 0 6.629 1zM6 7.75a.75.75 0 0 0 0 1.5h4a.75.75 0 1 0 0-1.5z"
+    />
+  </svg>
+)
+export default SvgFolderMinus
