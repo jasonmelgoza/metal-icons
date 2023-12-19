dc10f2bbd2524e820389dd6086979fa464163a87
Jason Melgoza <jasonmelgoza@gmail.com>
2023-12-19 22:04:02 +0000

    feat: Added export of LayoutRows in metal-icons
    Added export of LayoutRows in metal-icons for 16 and 24 pixels

---
+++ packages/metal-icons/src/16/solid/LayoutRows.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+const SvgLayoutRows = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={16}
+    fill="currentColor"
+    {...props}
+  >
+    <path d="M16 3.25A2.25 2.25 0 0 0 13.75 1H2.25A2.25 2.25 0 0 0 0 3.25V4.5h16zM16 6H0v4h16zm0 5.5H0v1.25A2.25 2.25 0 0 0 2.25 15h11.5A2.25 2.25 0 0 0 16 12.75z" />
+  </svg>
+)
+export default SvgLayoutRows
