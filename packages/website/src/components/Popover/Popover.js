5577e3b641374298ea12dcdbf8533afe2e216703
Jason Melgoza <jasonmelgoza@gmail.com>
2023-12-21 05:46:44 +0000

    style: Update Popover styling
    Update Popover js and css files with new styling

--- packages/website/src/components/Popover/Popover.js
+++ packages/website/src/components/Popover/Popover.js
@@ -1,6 +1,9 @@
 import * as Popover from '@radix-ui/react-popover'
 import { Cog as IconCog } from 'metal-icons'
+import { Inter } from 'next/font/google'
 import Styles from './Popover.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 const IconInfoPopover = () => (
   <Popover.Root>
@@ -10,7 +13,11 @@ const IconInfoPopover = () => (
       </button>
     </Popover.Trigger>
     <Popover.Portal>
-      <Popover.Content className="PopoverContent" sideOffset={5}>
+      <Popover.Content
+        className={`${inter.className} ${Styles.PopoverContent}`}
+        sideOffset={5}
+        align="end"
+      >
         <div>Foo content</div>
       </Popover.Content>
     </Popover.Portal>
