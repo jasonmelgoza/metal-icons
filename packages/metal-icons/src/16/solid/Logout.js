a9adea7541dc09630c0d2819c4275acdedd2051f
Jason Melgoza <jasonmelgoza@gmail.com>
2023-12-28 16:28:01 +0000

    feat: Add Logout icon
    Added the Logout icon to the metal-icons package in both 16px and 24px sizes, and in both outline and solid styles.

---
+++ packages/metal-icons/src/16/solid/Logout.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+const SvgLogout = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={16}
+    fill="currentColor"
+    {...props}
+  >
+    <path d="M6.5 1.75A.75.75 0 0 0 5.75 1h-2.5A2.25 2.25 0 0 0 1 3.25v9.5A2.25 2.25 0 0 0 3.25 15h2.5a.75.75 0 1 0 0-1.5h-2.5a.75.75 0 0 1-.75-.75v-9.5a.75.75 0 0 1 .75-.75h2.5a.75.75 0 0 0 .75-.75" />
+    <path d="M9.97 3.72a.75.75 0 0 1 1.06 0l3.5 3.5a.75.75 0 0 1 0 1.06l-3.5 3.5a.75.75 0 1 1-1.06-1.06l2.22-2.22H6A.75.75 0 0 1 6 7h6.19L9.97 4.78a.75.75 0 0 1 0-1.06" />
+  </svg>
+)
+export default SvgLogout
