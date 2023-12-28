9a349f35fae437d8422d59b5bcebef5d5ec406b2
Jason Melgoza <jasonmelgoza@gmail.com>
2023-12-28 16:51:18 +0000

    feat: Added Minimize export
    Added Minimize export in metal-icons for 16px and 24px, both outline and solid styles.

---
+++ packages/metal-icons/src/16/outline/Minimize.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+const SvgMinimize = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={16}
+    fill="currentColor"
+    {...props}
+  >
+    <path d="M10 4.94V2.75a.75.75 0 0 0-1.5 0v4c0 .414.336.75.75.75h4a.75.75 0 1 0 0-1.5h-2.19l2.72-2.72a.75.75 0 0 0-1.06-1.06zM2.75 8.5a.75.75 0 0 0 0 1.5h2.19l-2.72 2.72a.75.75 0 1 0 1.06 1.06L6 11.06v2.19a.75.75 0 1 0 1.5 0v-4a.75.75 0 0 0-.75-.75z" />
+  </svg>
+)
+export default SvgMinimize
