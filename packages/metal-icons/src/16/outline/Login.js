5b267b19a9eeb7a18c9c8ffc2af2de8f149aaff4
Jason Melgoza <jasonmelgoza@gmail.com>
2023-12-28 16:22:38 +0000

    fix: New Login icon added
    Added new Login icon in 16px and 24px sizes in both outline and solid styles

---
+++ packages/metal-icons/src/16/outline/Login.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+const SvgLogin = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={16}
+    fill="currentColor"
+    {...props}
+  >
+    <path d="M9.5 1.75a.75.75 0 0 1 .75-.75h2.5A2.25 2.25 0 0 1 15 3.25v9.5A2.25 2.25 0 0 1 12.75 15h-2.5a.75.75 0 1 1 0-1.5h2.5a.75.75 0 0 0 .75-.75v-9.5a.75.75 0 0 0-.75-.75h-2.5a.75.75 0 0 1-.75-.75" />
+    <path d="M5.97 3.72a.75.75 0 0 1 1.06 0l3.5 3.5a.75.75 0 0 1 0 1.06l-3.5 3.5a.75.75 0 0 1-1.06-1.06L8.19 8.5H2A.75.75 0 0 1 2 7h6.19L5.97 4.78a.75.75 0 0 1 0-1.06" />
+  </svg>
+)
+export default SvgLogin
