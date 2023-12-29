eb8b5c869040cbc5889a9900a9d385ef170b4e2e
Jason Melgoza <jasonmelgoza@gmail.com>
2023-12-28 16:25:19 -0800

    Version bump

--- packages/website/src/components/Popover/Popover.js
+++ packages/website/src/components/Popover/Popover.js
@@ -87,7 +87,7 @@ export default function InfoPopover() {
       </section>
       <section>
         <h2>Version</h2>
-        <p>0.1.18</p>
+        <p>0.1.19</p>
       </section>
     </MetalPopover>
   )
