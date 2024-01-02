ad26448c128fc49d716fc308d6b5a3aa59aafd9e
Jason Melgoza <jasonmelgoza@gmail.com>
2024-01-02 17:20:28 +0000

    feat: Add Search icon export
    Added export for Search icon in metal-icons at 16px and 24px sizes.

---
+++ packages/metal-icons/src/24/solid/Search.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+const SvgSearch = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="currentColor"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      d="M10.5 2a8.5 8.5 0 1 0 5.457 15.017l4.763 4.763a.751.751 0 1 0 1.06-1.06l-4.763-4.763A8.5 8.5 0 0 0 10.5 2m-7 8.5a7 7 0 1 1 14 0 7 7 0 0 1-14 0"
+    />
+  </svg>
+)
+export default SvgSearch
