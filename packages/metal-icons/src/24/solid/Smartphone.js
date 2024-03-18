5e84de801f2c48f7731c7fd09acacec0fa5abbb3
Jason Melgoza <jasonmelgoza@gmail.com>
2024-03-17 21:48:36 -0700

    Add new icons: Smartphone and Star

---
+++ packages/metal-icons/src/24/solid/Smartphone.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+const SvgSmartphone = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="currentColor"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      d="M6.75 1A2.75 2.75 0 0 0 4 3.75v16.5A2.75 2.75 0 0 0 6.75 23h10.5A2.75 2.75 0 0 0 20 20.25V3.75A2.75 2.75 0 0 0 17.25 1zM12 18a1 1 0 0 0 0 2h.01a1 1 0 0 0 0-2z"
+    />
+  </svg>
+)
+export default SvgSmartphone
