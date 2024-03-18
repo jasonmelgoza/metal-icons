c19ee576c523a9c67b00ae11efc2162e725bcbdb
Jason Melgoza <jasonmelgoza@gmail.com>
2024-03-17 21:56:26 -0700

    Add swap-horizontal icons in different sizes and styles

---
+++ packages/metal-icons/src/24/outline/SwapHorizontal.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+const SvgSwapHorizontal = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="currentColor"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      d="M6.72 3.309a.75.75 0 0 0-1-1.118l-4.47 4a.75.75 0 0 0 0 1.118l4.47 4a.75.75 0 0 0 1-1.118L3.713 7.5H22.25a.75.75 0 0 0 0-1.5H3.713zm10.56 10.5a.75.75 0 1 1 1-1.118l4.47 4a.75.75 0 0 1 0 1.118l-4.47 4a.75.75 0 1 1-1-1.118L20.287 18H1.75a.75.75 0 0 1 0-1.5h18.537z"
+    />
+  </svg>
+)
+export default SvgSwapHorizontal
