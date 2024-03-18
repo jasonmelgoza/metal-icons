bcac81259399b23cf5f9c10bb6940cd2044bd58d
Jason Melgoza <jasonmelgoza@gmail.com>
2024-03-17 22:07:46 -0700

    Add SwapVertical icon to metal-icons package

---
+++ packages/metal-icons/src/24/solid/SwapVertical.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+const SvgSwapVertical = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="currentColor"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      d="M20.691 6.72a.75.75 0 1 0 1.118-1l-4-4.47a.75.75 0 0 0-1.118 0l-4 4.47a.75.75 0 0 0 1.118 1L16.5 3.713V22.25a.75.75 0 0 0 1.5 0V3.713zm-10.5 10.56a.75.75 0 1 1 1.118 1l-4 4.47a.75.75 0 0 1-1.118 0l-4-4.47a.75.75 0 1 1 1.118-1L6 20.287V1.75a.75.75 0 0 1 1.5 0v18.537z"
+      opacity={0.9}
+    />
+  </svg>
+)
+export default SvgSwapVertical
