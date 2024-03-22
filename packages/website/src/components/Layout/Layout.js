9abda9b46213629c8196bdb3700577786a1aa681
Jason Melgoza <jasonmelgoza@gmail.com>
2024-03-22 16:12:46 -0700

    Update metal-icons package version and add homepage and author information

--- packages/website/src/components/Layout/Layout.js
+++ packages/website/src/components/Layout/Layout.js
@@ -90,7 +90,7 @@ export default function Layout({ children }) {
                 href="https://github.com/jasonmelgoza/metal-icons"
                 target="_blank"
               >
-                metal-icons v0.1.24
+                metal-icons v0.1.25
               </a>
             </p>
           </div>
