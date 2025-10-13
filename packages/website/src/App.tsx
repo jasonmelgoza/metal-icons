e2ab78d77b0c18785b07686fa522a248babc3d9b
Jason Melgoza <jasonmelgoza@gmail.com>
2025-10-12 17:09:37 -0700

    Refactor toast notifications by creating a ToastProvider component, simplifying App component structure and improving code organization.

--- packages/website/src/App.tsx
+++ packages/website/src/App.tsx
@@ -1,41 +1,11 @@
 import { FC } from 'react'
-import { Toaster } from 'sonner'
 import Home from './pages/Home'
-import Styles from './styles/App.module.css'
+import { ToastProvider } from './components'
 
 const App: FC = () => (
   <>
     <Home />
-    <Toaster
-      position="bottom-center"
-      style={
-        {
-          '--front-toast-height': '55px',
-          '--width': '356px',
-          '--gap': '14px',
-          '--offset-top': '24px',
-          '--offset-right': '24px',
-          '--offset-bottom': '24px',
-          '--offset-left': '24px',
-          '--mobile-offset-top': '16px',
-          '--mobile-offset-right': '16px',
-          '--mobile-offset-bottom': '16px',
-          '--mobile-offset-left': '16px',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        } as React.CSSProperties
-      }
-      toastOptions={{
-        unstyled: true,
-        classNames: {
-          toast: Styles['toast-container'],
-          content: Styles['toast-content'],
-          title: Styles['toast-title'],
-          description: Styles['toast-description'],
-        },
-      }}
-    />
+    <ToastProvider />
   </>
 )
 
